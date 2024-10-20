d3aaca00a17ef5548087c238205ec50e082a641e
Isenewo Oluwaseyi Ephraim <isenewoephr2012@gmail.com>
2024-10-20 01:29:20 +0100

    edited states folder

--- src/states/changingBgImages.js
+++ src/states/changingBgImages.js
@@ -1,18 +1,10 @@
-import training_students from "./assets/image/training_students.jpg"
-import training_students_2 from "./assets/images/training_students_2.jpg"
-import training_students_3 from "./assets/images/training_students_3.jpg"
-
-// export const changingBgImages = [
-//     "../assets/images/training_students.jpg", 
-//     "../assets/images/training_students_2.jpg", 
-//     "../assets/images/training_students_3.webp"
-// ];
-
 export const changingBgImages = [
-    training_students, 
-    training_students_2,
-    training_students_3
+    "../assets/images/training_students.jpg", 
+    "../assets/images/training_students_2.jpg", 
+    "../assets/images/training_students_3.webp"
 ];
+
+
 
 export const changingTextColors = ["#FFFFFF", "#5541D7", "#11142D"];
 
